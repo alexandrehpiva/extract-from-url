9d5989c80866c5a2e5407206d91a51d0d1a83993
alexandrehpiva <alexandrehpiva@gmail.com>
2020-11-09 21:42:20 -0300

    Improve tests

--- src/__tests__/extractFromUrl.test.ts
+++ src/__tests__/extractFromUrl.test.ts
@@ -1,8 +1,8 @@
 import { Url } from '../index';
 import extractFromUrl from '../../dist/index';
 
-describe('Extrai partes de uma url', () => {
-  test('Deve extrair corretamente todas as partes de uma url válida', () => {
+describe('Extract parts of a url', () => {
+  it('Should correctly extract all parts of a valid url', () => {
     const url = 'https://www.subdomain.domain.com:80/path/123';
     const parts: Url = {
       protocol: 'https',
@@ -13,6 +13,38 @@ describe('Extrai partes de uma url', () => {
       subdomain: 'subdomain',
       domain: 'domain.com',
       path: '/path/123',
+    };
+
+    expect(extractFromUrl(url)).toEqual(parts);
+  });
+
+  it('Should correctly extract all parts of a url without subdomain', () => {
+    const url = 'https://www.googleapis.com/youtube/v3/search';
+    const parts: Url = {
+      protocol: 'https',
+      address: 'www.googleapis.com',
+      port: undefined,
+      ip: undefined,
+      hostname: 'www.googleapis.com',
+      subdomain: undefined,
+      domain: 'googleapis.com',
+      path: '/youtube/v3/search',
+    };
+
+    expect(extractFromUrl(url)).toEqual(parts);
+  });
+
+  it('Should correctly extract all parts of a url with only ip + port', () => {
+    const url = 'http://127.0.0.1:3000';
+    const parts: Url = {
+      protocol: 'http',
+      address: '127.0.0.1',
+      port: '3000',
+      ip: '127.0.0.1',
+      hostname: undefined,
+      subdomain: undefined,
+      domain: undefined,
+      path: undefined,
     };
 
     expect(extractFromUrl(url)).toEqual(parts);
