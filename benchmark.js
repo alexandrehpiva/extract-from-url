22e122775be872428239ccb8b9e7c07ef542b28b
Alexandre Piva <alexandrehpiva@gmail.com>
2021-07-18 10:38:51 -0300

    Add benchmark tests to compare

--- benchmark.js
+++ benchmark.js
@@ -4,6 +4,24 @@ const { default: extractFromUrl } = require('./dist/index');
 const times = 100
 const timesToLocale = times.toLocaleString()
 
+function extractFromUrlRegEx(url) {
+  const matches = url.match(/([^\/?#.]+\.[^\/?#.:]+)(?:[\/?#:]|$)/i)
+
+  return matches?.[1]
+}
+
+function extractFromUrlArray(url) {
+  let domain
+
+  if (url.indexOf('://') > -1) {
+    domain = url.split('/')[2]
+  } else {
+    domain = url.split('/')[0]
+  }
+
+  return domain.split(':')[0].replace('www.', '')
+}
+
 const url = 'https://www.npmjs.com/package/extract-from-url'
 
 bench(`extractFromUrl: extract domain ${timesToLocale} times`, (b) => {
@@ -11,6 +29,26 @@ bench(`extractFromUrl: extract domain ${timesToLocale} times`, (b) => {
 
   for (let i = 0; i < times; i++) {
     extractFromUrl(url, 'domain')
+  }
+
+  b.end()
+})
+
+bench(`extractFromUrlRegEx: extract domain regex ${timesToLocale} times`, (b) => {
+  b.start()
+
+  for (let i = 0; i < times; i++) {
+    extractFromUrlRegEx(url)
+  }
+
+  b.end()
+})
+
+bench(`extractFromUrlArray: extract domain array hack ${timesToLocale} times`, (b) => {
+  b.start()
+
+  for (let i = 0; i < times; i++) {
+    extractFromUrlArray(url)
   }
 
   b.end()
